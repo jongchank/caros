9b58f95e1b6272c02644fe9a7fb6d414ad2a179d
Jong-Chan Kim <jongchank@gmail.com>
2025-04-13 15:17:11 +0900

    Add files for lecture 12

---
+++ 12/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "IfxCan_regdef.h"
+#include "IfxCan_reg.h"
+
+Ifx_CAN dummy;
+
+#undef MODULE_CAN
+#define MODULE_CAN dummy
+
+int main(void) {
+
+    return 0;
+}
